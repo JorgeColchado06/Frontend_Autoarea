f246592aaad9b06acfe1e6c2c93026b925e570fd
Rodrigo Espino <rodrigosantacruz42@gmail.com>
2023-08-18 01:35:01 -0600

    History Purchases

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import App from "./App";
 import ProductProvider from "./contexts/ProductContext";
 import SidebarProvider from "./contexts/SidebarContext";
 import CartProvider from "./contexts/CartContext";
+import PurchasesProvider from "./contexts/PurchasesContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SidebarProvider>
     <CartProvider>
+      <PurchasesProvider>
       <ProductProvider>
         <React.StrictMode>
           <App />
         </React.StrictMode>
       </ProductProvider>
+      </PurchasesProvider>
     </CartProvider>
   </SidebarProvider>
 );
